fdf8d6ed633e38671c62a454b465a2b977a9f3d6
Satyam <satyam19arya@gmail.com>
2023-09-16 21:24:05 +0530

    testing

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT;
 dbConnect();
 
 app.get('/', (req, res) => {
-    res.send('Hello from server 😎');
+    res.send('Hello from PRANAV 😎');
 });
 
 app.use('/auth', authRouter);
